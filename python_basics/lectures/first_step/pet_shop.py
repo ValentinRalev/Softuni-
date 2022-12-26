438950f8b3bd198203160f9c76d6724314312eb7
ValentinRalev <121321655+ValentinRalev@users.noreply.github.com>
2022-12-27 01:09:50 +0200

    Add files via upload

---
+++ python_basics/lectures/first_step/pet_shop.py
@@ -0,0 +1,4 @@
+dog_food = int(input())
+cat_food = int(input())
+money_needed = (dog_food * 2.50) + (cat_food * 4.00)
+print(f"{money_needed} lv.")
---
+++ python_basics/lectures/first_step/yard_greening.py
@@ -0,0 +1,6 @@
+area = float(input())
+area_cost = area * 7.61
+total_cost = area_cost * 0.82
+discount = area_cost * 0.18
+print(f"The final price is: {total_cost:.2f} lv.")
+print(f"The discount is: {discount} lv.")
