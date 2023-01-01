704367510a9d8576c33d4de08565ae7873affbd0
ValentinRalev <valentin_ralev@yahoo.com>
2023-01-01 22:18:50 +0200

    solved

---
+++ python_advanced/lectures/lists_as_stacks_and_queues/hot_potato.py
@@ -0,0 +1,9 @@
+from collections import deque
+names = deque(input().split())
+toss = int(input())
+while len(names) > 1:
+    names.rotate(-toss)
+    print(f"Removed {names.pop()}")
+print(f"Last is {names.pop()}")
+
+
