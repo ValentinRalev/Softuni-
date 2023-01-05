1aba6d410885a4af2f71e2bdccbbb9d44c4fef3e
ValentinRalev <valentin_ralev@yahoo.com>
2023-01-05 15:22:43 +0200

    solved

---
+++ python_advanced/exams/tuple_and_sets/longest_intersection.py
@@ -0,0 +1,18 @@
+number = int(input())
+longest_len = 0
+result = set()
+for _ in range(number):
+    first, second = input().split("-")
+    first_start, first_end = [int(x) for x in first.split(",")]
+    second_start, second_end = [int(y) for y in second.split(",")]
+    first_set = set()
+    second_set = set()
+    for nums in range(first_start, first_end +1):
+        first_set.add(nums)
+    for nums in range(second_start, second_end +1):
+        second_set.add(nums)
+    res = first_set.intersection(second_set)
+    if longest_len < len(res):
+        longest_len = len(res)
+        result = res
+print(f"Longest intersection is [{', '.join(str(x) for x in result)}] with length {longest_len}")
